47b3636a130b75629468afb9ed2d97f6ef7e134a
klforthwind <kl.forthwind@gmail.com>
2019-05-25 23:14:02 +0000

    delete old method of data handling

--- www/blog/script.js
+++ www/blog/script.js
@@ -11,11 +11,6 @@ function loadPost() {
     request.onload = function () {
         var data = this.response;
         if (request.status >= 200 && request.status < 400) {
-            // const p = document.createElement('p');
-            // p.className = "floatLeft";
-            // p.textContent = data.substring(1, data.length-2);
-            // app.appendChild(p);
-
             let r = document.getElementById('root');
             r.innerHTML = data.substring(1, data.length-2);
         }
