8f58133ab5beef6f7634c9c27a079f8695a07070
klforthwind <kl.forthwind@gmail.com>
2019-05-25 04:30:47 +0000

    begin designing the blog layout

--- www/blog/script.js
+++ www/blog/script.js
@@ -1,16 +1,18 @@
 function loadPost(name) {
+    if (name==undefined) {
+        name = "";
+    }
+    const app = document.getElementById('root');
+
     var request = new XMLHttpRequest();
     var restAPI = 'https://www.klforthwind.com:4242/blog/' + name;
     request.open('GET', restAPI, true);
     request.onload = function () {
         var data = this.response;
         if (request.status >= 200 && request.status < 400) {
-            document.write(data);
-        } else {
-            // const errorMessage = document.createElement('marquee');
-            // errorMessage.textContent = "Rawr XD, this page just got rawr xdeeed!";
-            // document.write(errorMessage);
-            console.log("RIP");
+            const p = document.createElement('p');
+            p.textContent = data;
+            app.appendChild(p);
         }
     }
 
