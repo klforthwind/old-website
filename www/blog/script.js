f1c1f880315f1db8e00a49ed20d4f08b3c64abdb
klforthwind <kl.forthwind@gmail.com>
2019-05-25 02:16:17 +0000

    continue work

--- www/blog/script.js
+++ www/blog/script.js
@@ -1,9 +1,16 @@
 function loadPost(name) {
     var request = new XMLHttpRequest();
-    var restAPI = 'https://klforthwind.com:4242/blog/' + name;
+    var restAPI = 'klforthwind.com:4242/blog/' + name;
     request.open('GET', restAPI, true);
     request.onload = function () {
-
+        var data = this.response;
+        if (request.status >= 200 && request.status < 400) {
+            document.write(data);
+        } else {
+            const errorMessage = document.createElement('marquee');
+            errorMessage.textContent = "Rawr XD, this page just got rawr xdeeed!";
+            document.write(errorMessage);
+        }
     }
 
     request.send();
