44bbde07c9fdd0fce2cc90d81bdec7b521a505e2
klforthwind <kl.forthwind@gmail.com>
2019-05-25 04:08:58 +0000

    i want to cry

--- www/blog/script.js
+++ www/blog/script.js
@@ -1,15 +1,16 @@
 function loadPost(name) {
     var request = new XMLHttpRequest();
-    var restAPI = 'klforthwind.com:4242/blog/' + name;
+    var restAPI = 'https://www.klforthwind.com:4242/blog/' + name;
     request.open('GET', restAPI, true);
     request.onload = function () {
         var data = this.response;
         if (request.status >= 200 && request.status < 400) {
             document.write(data);
         } else {
-            const errorMessage = document.createElement('marquee');
-            errorMessage.textContent = "Rawr XD, this page just got rawr xdeeed!";
-            document.write(errorMessage);
+            // const errorMessage = document.createElement('marquee');
+            // errorMessage.textContent = "Rawr XD, this page just got rawr xdeeed!";
+            // document.write(errorMessage);
+            console.log("RIP");
         }
     }
 
