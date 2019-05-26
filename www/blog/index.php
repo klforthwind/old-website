c8feee96616b26c6e40272badba00a46526d2ec4
klforthwind <kl.forthwind@gmail.com>
2019-05-26 05:17:10 +0000

    minor changes

--- www/blog/index.php
+++ www/blog/index.php
@@ -4,12 +4,12 @@
 	<head>
 		<meta charset="utf-8">
 
-        <title>klforthwind / forthwind</title>
+        <title>Forthwind</title>
         <link rel="stylesheet" href="style.css" />
 		<link rel="shortcut icon" href="../24x24Forthwind.jpg"/> 
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Links to klforthwind's / forthwind's stuff" />
+        <meta name="description" content="Links to Forthwind's stuff" />
 
 		<!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135031848-1"></script>
@@ -17,8 +17,8 @@
 	</head>
 	<body>
 		<section id="container">
-			<a href="/blog/"><img src="../400x400Forthwind.jpg" alt="Forthwind / klforthwind" height="128px" width="128px"></a>
-			<h1>klforthwind's blog</h1>
+			<a href="/blog/"><img src="../400x400Forthwind.jpg" alt="Forthwind" height="128px" width="128px"></a>
+			<h1>Forthwind's blog</h1>
 			<h2>Uhhh... I tried.</h2>
 			<!--Place to add blog articles below here-->
 			<div id="root"></div>
