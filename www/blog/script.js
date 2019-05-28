09913c398a14c321f0cbe13dc7887dfc885eb66b
klforthwind <kl.forthwind@gmail.com>
2019-05-28 21:01:49 +0000

    fix blog syntax

--- www/blog/script.js
+++ www/blog/script.js
@@ -21,6 +21,7 @@ xhr.onload = function () {
         data = data.replace(/<\/h1><br><h2>/g, "</h1><h2>");
         data = data.replace(/\bsh\\n/g, "");
         data = data.replace(/\\\"/g, "\"");
+        data = data.replace(/\\\\/g, "\\");
         r.innerHTML = data;
         
         let pic = document.getElementById("picture");
